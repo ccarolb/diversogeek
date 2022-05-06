ee31156cbec1c0ff8b816fcea49b1ec43ac4f641
Carolina Borges <carolinaborgescardoso@gmail.com>
2022-05-06 19:17:40 -0300

    cria página de contato e adiciona formulário

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    // Aqui os elementos que possuem a classe .hidden são escondidos no momento do carregamento da página
+    $(".hidden").hide();
+
+    // Aqui é definida uma função para, quando o usuário clicar no botão "Ler mais notícias", os elementos com a classe hidden são mostrados, e o botão "Ler mais notícias" é escondido (simulando que o usuário já carregou todos os posts.)
+    $(".ler-mais").click(function () {
+        $(".hidden").show();
+        $(".ler-mais").hide();
+    })
+
+
+    // Aqui é definida outra função para, quando a página for scrollada 500px para baixo, o botão de "Voltar ao topo" é mostrado. Do contrário, ele fica escondido.
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $("#volta-topo").css({ "display": "block" });
+        } else {
+            $("#volta-topo").css({ "display": "none" });
+        }
+    });
+
+    // Aqui a função que determina que o usuário voltará ao topo da página quando clicar no botão "Voltar ao topo".
+    $("#volta-topo").click(function () {
+        $("html").animate({ scrollTop: 0 }, 500);
+
+    })
+
+
+
+});
