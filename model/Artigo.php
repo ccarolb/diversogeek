b178849aa16432e30718633eeaa1560eb74fa88a
Carolina Borges Cardoso <20212GR.ADS0003@gravatai.ifsul.edu.br>
2022-09-09 21:35:59 -0300

    inicia implementação do cadastro de artigo

--- model/Artigo.php
+++ model/Artigo.php
@@ -7,24 +7,39 @@ class Artigo {
     private $titulo;
     private $resumo;
 
+    public function verificaArtigo($titulo) {
+        include('..'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'conexaoBd.php');
+
+        try {
+
+            $sql = $pdo->prepare("SELECT id_artigo FROM tb_artigos where nm_artigo ='".$titulo."'");
+            $sql->execute();
+            return $sql->fetchAll();
+
+        } catch(Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+        
+    }
     public function cadastrarArtigo() {
         include('..'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'conexaoBd.php');
         include_once ('..'.DIRECTORY_SEPARATOR.'controller'.DIRECTORY_SEPARATOR.'usuario.php');
 
         try {
             $artigo = $this->verificaArtigo($this->titulo);
-            $idUsuario = getUsuarioById();
+            $this->usuario = getUsuarioById();
 
             if(count($artigo) <= 0) {
 
-                $sql = $pdo->prepare("insert into tb_artigos(nm_usuario, ds_email, ds_senha) values(:nome, :email, MD5(:senha))");
-                $sql->bindValue(':nome', $this->nome, PDO::PARAM_STR);
-                $sql->bindValue(':email', $this->email, PDO::PARAM_STR);
-                $sql->bindValue(':senha', $this->senha, PDO::PARAM_STR);
+                $sql = $pdo->prepare("insert into tb_artigos(id_usuario, cd_imagem, nm_artigo, ds_artigo) values(:usuario, :imagem, :titulo, :resumo)");
+                $sql->bindValue(':usuario', $this->usuario, PDO::PARAM_INT);
+                $sql->bindValue(':imagem', $this->imagem, PDO::PARAM_INT);
+                $sql->bindValue(':titulo', $this->titulo, PDO::PARAM_STR);
+                $sql->bindValue(':resumo', $this->resumo, PDO::PARAM_STR);
                 $sql->execute();
-                echo 'Usuário cadastrado com sucesso.';
+                echo 'Artigo postado com sucesso.';
             } else {
-                echo 'Esse usuário já existe.';
+                echo 'Não foi possível postar o artigo.';
             }
         } catch(Exception $e) {
             throw new Exception($e->getMessage());
